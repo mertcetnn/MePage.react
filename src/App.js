9af854cfea4d7d24c8e1345f33d3ea3e93f7955d
MertCetin <mert.cetnn@gmail.com>
2022-11-10 13:34:07 -0500

    having error

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header/Header";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects/Projects";
+
 function App() {
   return (
     <>
