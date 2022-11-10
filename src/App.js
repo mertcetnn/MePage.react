c59e359ddd5f2b3bac96bafcb5f55a0f0ed2abf3
MertCetin <mert.cetnn@gmail.com>
2022-11-10 13:18:11 -0500

    routes edited

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
