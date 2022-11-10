e419a79cff11277160edf227ac572223c40eb6c1
MertCetin <mert.cetnn@gmail.com>
2022-11-10 18:22:04 -0500

    Updates

---
+++ precache-manifest.498e5e4c438011713a89ca972ca0d963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "911d9038ba65b880b3f1",
+    "url": "/static/css/main.7d45b1f9.chunk.css"
+  },
+  {
+    "revision": "911d9038ba65b880b3f1",
+    "url": "/static/js/main.911d9038.chunk.js"
+  },
+  {
+    "revision": "47894be22fe098402c04",
+    "url": "/static/js/1.47894be2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5b9983b576ca4a24cd1053fcdad3cea",
+    "url": "/static/media/me.d5b9983b.jpg"
+  },
+  {
+    "revision": "63eaff915722c82bc4997868c537718d",
+    "url": "/index.html"
+  }
+];
